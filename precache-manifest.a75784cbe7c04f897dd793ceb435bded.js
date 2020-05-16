985a854af3100161545764d8f1e4f217ced405ab
Chandana123 <chandana@Chandana123s-MacBook-Air.local>
2020-05-16 13:35:41 +0530

    Updates

---
+++ precache-manifest.a75784cbe7c04f897dd793ceb435bded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f7d3d7e41b452ef6ca6e2c3e08242d",
+    "url": "/rick-and-morty-surya-app/index.html"
+  },
+  {
+    "revision": "49e288a45ca91097c942",
+    "url": "/rick-and-morty-surya-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e7482874419519cf0e0d",
+    "url": "/rick-and-morty-surya-app/static/css/main.f5eb38ad.chunk.css"
+  },
+  {
+    "revision": "49e288a45ca91097c942",
+    "url": "/rick-and-morty-surya-app/static/js/2.30fe054e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rick-and-morty-surya-app/static/js/2.30fe054e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7482874419519cf0e0d",
+    "url": "/rick-and-morty-surya-app/static/js/main.014517b5.chunk.js"
+  },
+  {
+    "revision": "55db68f6d42794b13d02",
+    "url": "/rick-and-morty-surya-app/static/js/runtime-main.c2737ded.js"
+  }
+]);
